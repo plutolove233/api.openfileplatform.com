b83cf238a37e1cc368693f4617e715854917fed1
李瑞阳 <lryself@qq.com>
2022-01-21 16:49:51 +0800

    修正Get错误

--- internal/globals/codes/error_codes.go
+++ internal/globals/codes/error_codes.go
@@ -8,20 +8,25 @@ const (
 	OperationFailure   = "1009" // 操作失败
 
 	// 通用错误
-	OK        = "200"  // Success
+	OK        = "2000" // Success
 	NotData   = "2001" // 没有数据
 	DataExist = "2002" // 数据已存在
 	DataError = "2003" // 数据错误
 
 	// 网络级错误
-	ParameterIllegal = "400" // 参数不合法
-	RequestOverDue   = "402" // 请求已过期
-	AccessDenied     = "403" // 拒绝访问
-	RoutingNotExist  = "404" // 路由不存在
-	RequestError     = "406" // 非法访问
-	IPError          = "407" // IP受限
+	ParameterIllegal = "4000" // 参数不合法
+	RequestOverDue   = "4001" // 请求已过期
+	LoginError       = "4002" // 登录已过期
+	AccessDenied     = "4003" // 拒绝访问
+	RoutingNotExist  = "4004" // 路由不存在
+	PasswordError    = "4005" // 密码错误
+	RequestError     = "4006" // 非法访问
+	IPError          = "4007" // IP受限
 
 	// 系统级错误
-	InternalError = "500"  // 系统错误
+	InternalError = "5000" // 系统错误
 	DBError       = "5001" // 数据库错误
+	ThirdError    = "5002" // 第三方系统错误
+	IOError       = "5003" // IO错误
+	UnKnownError  = "5004" // 未知错误
 )
